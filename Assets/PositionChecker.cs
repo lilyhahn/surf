8a1f3a93efcee49c3af9e3ffd12aeaf7d054b736
Toby Hahn <thbike17@gmail.com>
2013-03-30 11:14:29 -0400

    added original textures, player now starts directly on terrain

--- Assets/PositionChecker.cs
+++ Assets/PositionChecker.cs
@@ -5,7 +5,10 @@ public class PositionChecker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject fps = GameObject.Find("First Person Controller");
+		float tempPos = Terrain.activeTerrain.SampleHeight(fps.transform.position);
+		tempPos += 5; //a sloppy fix, it seems to be placing the controller inside the terrain most of the time
+		fps.transform.position = new Vector3(fps.transform.position.x, tempPos, fps.transform.position.z);
 	}
 	
 	// Update is called once per frame

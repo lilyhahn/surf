8d1f8f12c7f08f95f475ad7066e2822c8fec44a8
Toby Hahn <thbike17@gmail.com>
2013-03-30 18:30:57 -0400

    added a menu and an icon and lots of good stuff

--- Assets/GuiScript.cs
+++ Assets/GuiScript.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 using System.Collections;
 
 public class GuiScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	public Texture2D icon;
+	public Texture2D button;
+	float iconres;
+	float buttonres;
 	
+	void Start(){
+		if(Screen.height >= 768){
+			iconres = 256;
+			buttonres = 256;
+		}
+		else if(Screen.height >= 480){
+			iconres = 128;
+			buttonres = 128;
+		}
+		else{
+			iconres = 64;
+			buttonres = 128;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnGUI(){
+		GUI.DrawTexture(new Rect((Screen.width/2 - (iconres / 2)), 40, iconres, iconres), icon, ScaleMode.ScaleAndCrop, false, 0);
+		if(GUI.Button(new Rect((Screen.width/2 - (buttonres / 2)), (Screen.height - buttonres) - 40, buttonres, buttonres), button)){
+			Application.LoadLevel("main");
+		}
+		
 	}
 }
